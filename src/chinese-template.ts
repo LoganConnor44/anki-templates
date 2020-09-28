b10b349ff85239ec4835dd64b9ef2334cecbd6b4
Logan Connor <loganconnor44@gmail>
2020-09-28 14:10:09 -0400

    simplified logic added original js file from template that i am rewriting

--- src/chinese-template.ts
+++ src/chinese-template.ts
@@ -146,6 +146,10 @@ class Vowel {
     }
 }
 
+class Zhuyin {
+
+}
+
 let vowels: Vowel[] = new Array<Vowel>();
 vowels[0] = new Vowel('a');
 vowels[1] = new Vowel('e');
@@ -154,85 +158,81 @@ vowels[3] = new Vowel('o');
 vowels[4] = new Vowel('u');
 
 const isPinyin = (value: string): boolean => toBoolean(value.search(/^[a-zA-Z0-9\s]*$/));
-const containsNumeric = (value: string): boolean => toBoolean(value.search(/\d/));
-const containsSpace = (value: string): boolean => toBoolean(value.search(/^(.*\s.*){1,}$/));
 const toBoolean = (value: number): boolean => value >= 0 ? true : false;
 
-const convertFromSyllable = (arrayOfLettersNumber: string[]): string => {
+
+const processPinyin = (value: string): string => {
+    value = value.trim();
+    return convertNumberedPinyinToAccented(value);
+};
+
+const retrieveVowelsBeforeNumber = (value: string): Array<string> => {
+    const vowelPattern = /([a|e|i|o|u])/i;
+    const regEx = new RegExp(vowelPattern);
+    const valueArrayReversed = value.split('').reverse();
+    let vowelsPresent: Array<string> = new Array<string>();
+    let initialConsonantsPassed: boolean = false;
+    valueArrayReversed.forEach(x => {
+        if (vowelsPresent.length > 0) {
+            initialConsonantsPassed = true;
+        }
+        if (regEx.test(x) || !initialConsonantsPassed) {
+            vowelsPresent.push(x);
+        }
+    });
+    return vowelsPresent.reverse();
+};
+
+const createAppropriateVowelWitchAccent = (valueArray: Array<string>, toneNumber: number): Vowel => {
     let vowelToBeAccented: string = '';
     vowels.forEach(vowel => {
-        arrayOfLettersNumber.forEach((letter: string) => {
-            if (letter === vowel.getVowel() && vowelToBeAccented === '') {
+        valueArray.forEach((x: string) => {
+            if (x === vowel.getVowel() && vowelToBeAccented === '') {
                 vowelToBeAccented = vowel.getVowel();
             }
         });
     });
-    const wholeWordAgain: string = arrayOfLettersNumber.join('');
-    const toneNumber: number = parseInt(wholeWordAgain.substring(wholeWordAgain.length - 1));
-    const vowelAccented: Vowel = new Vowel(vowelToBeAccented);
-    vowelAccented.setTone(toneNumber);
-    const valueWithAccentAndNumber: string = wholeWordAgain.replace(
-        vowelToBeAccented,
-        vowelAccented.getVowelWithTone()
+    const accentedVowel = new Vowel(vowelToBeAccented);
+    accentedVowel.setTone(toneNumber);
+    return accentedVowel;
+};
+
+const replaceVowelWithAccentedVowel = (value: string, accentedVowel: Vowel): string => {
+    const valueReversed: string = value.split('').reverse().join('');
+    const valueWithAccentReversed: string = valueReversed.replace(
+        accentedVowel.getVowel(),
+        accentedVowel.getVowelWithTone()
     );
-    const valueWithOutNumber: string = valueWithAccentAndNumber.replace(toneNumber.toString(), '');
-    return valueWithOutNumber;
+    return valueWithAccentReversed.split('').reverse().join('');
 };
 
-const separateSyllables = (arrayOfLettersNumber: string[]): string[] => {
-    let syllables: string[] = new Array<string>();
-    let startIndex = 0;
-    for (let i = 0; i < arrayOfLettersNumber.length; i++) {
-        if (containsNumeric(arrayOfLettersNumber[i]) ||  arrayOfLettersNumber.length - 1 === i) {
-            const syllable = arrayOfLettersNumber.slice(startIndex, i + 1).join('');
-            syllables.push(syllable);
-            startIndex = i + 1;
-        }
-    }
-    return syllables;
-}
-
-const convertFromSingleWord = (arrayOfLettersNumber: string[]): string => {
-    let converted = '';
-    const syllables = separateSyllables(arrayOfLettersNumber)
-    syllables.forEach(syllable => {
-        converted += convertFromSyllable(syllable.split(''));
-    });
-    return converted;
-}
-
-const convertFromMultipleWords = (arrayOfWords: string[]): string => {
-    let convertedValueArray: string[] = new Array<string>();
-    arrayOfWords.forEach(word => {
-        if (convertedValueArray.length > 0) {
-            convertedValueArray.push(' ');
-        }
-        convertedValueArray.push(convertFromSingleWord(word.split('')));
-    });
-    return convertedValueArray.join('');
+const replaceNumberWithAccentedVowel = (value: string, toneNumber: number): string => {
+    const vowelsImmediatelyBeforeNumber: Array<string> = retrieveVowelsBeforeNumber(value);
+    const vowelAccented: Vowel = createAppropriateVowelWitchAccent(vowelsImmediatelyBeforeNumber, toneNumber);
+    return replaceVowelWithAccentedVowel(value, vowelAccented);
 };
 
-const convertNumberedToneToAccentTone = (value: string): string => {
-    let converted: string = '';
-    let separator: string = '';
-    if (containsSpace(value)) {
-        separator = ' ';
+const convertNumberedPinyinToAccented = (value: string): string => {
+    let convertedValue: string = '';
+    const minimumOneOrMaxSixLettersCaseInsensitive: RegExp = new RegExp(/([a-zA-Z]{1,6})/);
+    const numbersOneThroughFive: RegExp = new RegExp(/([1-5])/);
+    const zeroOrOneSpaceCharacter: RegExp = new RegExp(/(\s*)/);
+    const finalRegEx: RegExp = new RegExp(
+        minimumOneOrMaxSixLettersCaseInsensitive.source +
+        numbersOneThroughFive.source +
+        zeroOrOneSpaceCharacter.source
+    );
+    let results: RegExpExecArray;
+    while ((results = finalRegEx.exec(value)) !== null) {
+        value = value.substr(results.index + results[0].length);
+        const originalText: string = results[0];
+        const romanLetters: string = results[1];
+        const tone: number = parseInt(results[2]);
+        const spaceCharacter: string = results[3];
+        const accentedSyllable: string = replaceNumberWithAccentedVowel(romanLetters, tone);
+        convertedValue += accentedSyllable + spaceCharacter;
     }
-    const arrayOfValue: string[] = value.split(separator);
-
-    if (separator === ' ') {
-        converted = convertFromMultipleWords(arrayOfValue);
-    } else {
-        converted = convertFromSingleWord(arrayOfValue);
-    }
-
-    return converted;
-};
-
-const processPinyin = (value: string): string => {
-    value = value.toLowerCase();
-    value = value.trim();
-    return convertNumberedToneToAccentTone(value);
+    return convertedValue;
 };
 
 const hanziToPhoneticCharacters = (value: string): string => {
@@ -248,10 +248,5 @@ const hanziToPhoneticCharacters = (value: string): string => {
 // exports for unit tests
 export default {
     hanziToPhoneticCharacters,
-    processPinyin,
-    convertNumberedToneToAccentTone,
-    convertFromMultipleWords,
-    convertFromSingleWord,
-    separateSyllables,
-    convertFromSyllable
+    processPinyin
 };
