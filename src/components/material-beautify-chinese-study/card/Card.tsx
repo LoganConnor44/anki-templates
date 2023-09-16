79c02f7394d96d8a4d337397503feb6611806291
Logan Connor <loganconnor44@gmail.com>
2023-09-16 10:23:21 -0400

    styling as well as changing sentencePhonic to sentence-phonic on the render of the component

--- src/components/material-beautify-chinese-study/card/Card.tsx
+++ src/components/material-beautify-chinese-study/card/Card.tsx
@@ -1,66 +1,66 @@
 import { h, Prop, Component, Host } from '@stencil/core';
-import { JSXBase } from "@stencil/core/internal";
+import { JSXBase } from '@stencil/core/internal';
 
 @Component({
 	tag: 'material-beautify-card',
-    styleUrl: '../styles/card.css',
-    shadow: true
+	styleUrl: '../styles/card.css',
+	shadow: true,
 })
 export class Card {
-    @Prop()
+	@Prop()
 	public primaryVocab: string;
-    @Prop()
+	@Prop()
 	public secondaryVocab: string;
-    @Prop()
+	@Prop()
 	public vocabPhonic: string;
-    @Prop()
-    public sentence: string;
-    @Prop()
-    public secondarySentence: string;
-    @Prop()
-    public sentencePhonic: string;
-    @Prop()
-    public type: string;
-    @Prop()
-    public orientation: string;
-    @Prop()
-    public meaning: string;
-    @Prop()
-    public sentenceMeaning: string;
-    @Prop()
-    public primaryHanziType: string;
-    @Prop()
-    public phonicOrientation: string;
+	@Prop()
+	public sentence: string;
+	@Prop()
+	public secondarySentence: string;
+	@Prop()
+	public sentencePhonic: string;
+	@Prop()
+	public type: string;
+	@Prop()
+	public orientation: string;
+	@Prop()
+	public meaning: string;
+	@Prop()
+	public sentenceMeaning: string;
+	@Prop()
+	public primaryHanziType: string;
+	@Prop()
+	public phonicOrientation: string;
 
-    private _content: JSXBase.HTMLAttributes<HTMLDivElement>;
+	private _content: JSXBase.HTMLAttributes<HTMLDivElement>;
 
-    protected getContent() {
-        return this._content;
-    }
-    protected setContent() {
-        this._content =
-            <Host>
-                <material-beautify-content class={this.type.toLowerCase()}
-                    vocab={ this.primaryVocab }
-                    secondary-vocab={ this.secondaryVocab }
-                    phonic={ this.vocabPhonic }
-                    sentence={ this.sentence }
-                    secondary-sentence={ this.secondarySentence }
-                    sentencePhonic={ this.sentencePhonic }
-                    orientation={ this.orientation }
-                    meaning={ this.meaning }
-                    sentence-meaning={ this.sentenceMeaning }
-                    type={ this.type }
-                    phonic-orientation={ this.phonicOrientation } />
-                <material-beautify-type class={this.type.toLowerCase()} 
-                    card-type={ this.type }
-                    primary-hanzi-type={ this.primaryHanziType }/>
-            </Host>
-        ;        
-    }
+	protected getContent() {
+		return this._content;
+	}
+	protected setContent() {
+		this._content = (
+			<Host>
+				<material-beautify-content
+					class={this.type.toLowerCase()}
+					vocab={this.primaryVocab}
+					secondary-vocab={this.secondaryVocab}
+					phonic={this.vocabPhonic}
+					sentence={this.sentence}
+					secondary-sentence={this.secondarySentence}
+					sentence-phonic={this.sentencePhonic}
+					orientation={this.orientation}
+					meaning={this.meaning}
+					sentence-meaning={this.sentenceMeaning}
+					type={this.type}
+					phonic-orientation={this.phonicOrientation}
+				/>
+				<material-beautify-type class={this.type.toLowerCase()} card-type={this.type} primary-hanzi-type={this.primaryHanziType} />
+			</Host>
+		);
+	}
 
-    public render() {
-        this.setContent();
-        return this.getContent();
-    }
+	public render() {
+		this.setContent();
+		return this.getContent();
+	}
 }
