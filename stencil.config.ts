f919e15482b51dd198ca5c2b79b61855ece6aa67
Logan Connor <loganconnor44@gmail.com>
2023-10-14 09:30:58 -0400

    formatting

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,23 +1,23 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'beautify-chinese-study',
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-      dir: 'output',
-    },
-    {
-      type: 'www',
-      baseUrl: 'http://beautify-chinese-study',
-      serviceWorker: null,
-    },
-  ]
+	namespace: 'beautify-chinese-study',
+	outputTargets: [
+		{
+			type: 'dist',
+			esmLoaderPath: '../loader',
+		},
+		{
+			type: 'dist-custom-elements',
+		},
+		{
+			type: 'docs-readme',
+			dir: 'output',
+		},
+		{
+			type: 'www',
+			baseUrl: 'http://beautify-chinese-study',
+			serviceWorker: null,
+		},
+	],
 };
