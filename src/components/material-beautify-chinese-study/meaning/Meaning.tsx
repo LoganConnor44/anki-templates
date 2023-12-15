51825ee825536df2844de73b49a36d08b270c910
Logan Connor <loganconnor44@gmail.com>
2023-12-15 15:53:00 -0500

    adding ---- leftover stuffffff----

--- src/components/material-beautify-chinese-study/meaning/Meaning.tsx
+++ src/components/material-beautify-chinese-study/meaning/Meaning.tsx
@@ -1,47 +1,49 @@
-import { h, Component, Prop, Watch, Element } from "@stencil/core";
-import { JSXBase } from "@stencil/core/internal";
+import { h, Component, Prop, Watch, Element } from '@stencil/core';
+import { JSXBase } from '@stencil/core/internal';
 
 @Component({
 	tag: 'material-beautify-meaning',
-    styleUrl: '../styles/meaning.css',
-    shadow: true
+	styleUrl: '../styles/meaning.css',
+	shadow: true,
 })
 export class Meaning {
+	@Element()
+	private element: HTMLElement;
 
-    @Element() 
-    private element: HTMLElement;
-
-    @Prop()
+	@Prop()
 	public meaning: string;
-    @Prop()
-    public orientation: string;
-    @Prop()
-    public idForStyles: string;
+	@Prop()
+	public orientation: string;
+	@Prop()
+	public idForStyles: string;
 
-    private _content: JSXBase.HTMLAttributes<HTMLDivElement>;
+	private _content: JSXBase.HTMLAttributes<HTMLDivElement>;
 
-    /**
-     * This method is used it is just not called directly like the IDE may think it should be
-     */
-     @Watch('orientation')
-     // @ts-ignore
-     private showItems() : void {
-         const primaryItem = this.element.shadowRoot.querySelector('.meaning');
-         primaryItem.className = primaryItem.className.replace('no-show', 'fade-in');
-     }
-    
-    protected getContent() : JSXBase.HTMLAttributes<HTMLDivElement> {
-        return this._content;
-    }
-    protected setContent() : void {
-        this._content =
-            <p id={ this.idForStyles + '-meaning' } class='meaning no-show'>{ this.meaning }</p>
-        ;
-    }
+	/**
+	 * This method is used it is just not called directly like the IDE may think it should be
+	 */
+	@Watch('orientation')
+	// @ts-ignore
+	private showItems(): void {
+		const primaryItem = this.element.shadowRoot.querySelector('.meaning');
+		primaryItem.className = primaryItem.className.replace('no-show', 'fade-in');
+	}
 
-    public render() : JSXBase.HTMLAttributes<HTMLDivElement>{
-        this.setContent();
+	protected getContent(): JSXBase.HTMLAttributes<HTMLDivElement> {
+		return this._content;
+	}
 
-        return this.getContent();
-    }
+	protected setContent(): void {
+		this._content = (
+			<p id={this.idForStyles + '-meaning'} class="meaning no-show">
+				{this.meaning}
+			</p>
+		);
+	}
+
+	public render(): JSXBase.HTMLAttributes<HTMLDivElement> {
+		this.setContent();
+
+		return this.getContent();
+	}
 }
